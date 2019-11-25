5ae6168d88d2327c9b3eb2d1cf48403f8a9113d9
camiloXweird <49212380+camiloXweird@users.noreply.github.com>
2019-11-24 19:50:42 -0500

    Touch y modificacion en android

---
+++ New Unity Project/Assets/OcultarElelemntos.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OcultarElelemntos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject banana, bread, burguer, cheese, chicken, ice_cream, onion, pizza, meat, wine;
+    void Start()
+    {
+        banana = GameObject.Find("Banana");
+        banana.SetActive(false);
+        bread = GameObject.Find("Bread");
+        bread.SetActive(false);
+        burguer = GameObject.Find("Burger");
+        burguer.SetActive(false);
+        cheese = GameObject.Find("Cheese");
+        cheese.SetActive(false);
+        chicken = GameObject.Find("Chicken");
+        chicken.SetActive(false);
+        ice_cream = GameObject.Find("IceCream");
+        ice_cream.SetActive(false);
+        onion = GameObject.Find("Onion");
+        onion.SetActive(false);
+        pizza = GameObject.Find("Pizza");
+        pizza.SetActive(false);
+        meat = GameObject.Find("Meat");
+        meat.SetActive(false);
+        wine = GameObject.Find("Wine");
+        wine.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ New Unity Project/Assets/TouchBanana.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchBanana : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject banana;
+    void Start()
+    {
+        banana = GameObject.Find("Banana");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.touchCount > 0) && (Input.touches[0].phase == TouchPhase.Began))
+        {
+            Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(raycast, out raycastHit))
+            {
+                if (raycastHit.transform.gameObject.CompareTag("CuboBanana"))
+                {
+                    Debug.Log("Hola");
+                    banana.SetActive(true);
+                }
+            }
+        }
+    }
+}
